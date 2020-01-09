21a251d333dd442ac15106b5cf20784374ba98e6
shreayan98c <shreayan98c@gmail.com>
2020-01-09 15:57:12 +0530

    Creating the book details page

--- catalog/urls.py
+++ catalog/urls.py
@@ -21,5 +21,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
-    # path('book/<int:pk>', views.BookDetailViews.as_view(), name='book-detail'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
