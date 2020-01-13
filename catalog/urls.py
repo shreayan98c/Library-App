9ac6b5df541381db9fc388ff3b2ba5b1e480173d
shreayan98c <shreayan98c@gmail.com>
2020-01-13 18:20:39 +0530

    Created the librarians view lent books page

--- catalog/urls.py
+++ catalog/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('lentbooks/', views.LentBooksListView.as_view(), name='borrowed'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -55,3 +55,12 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
 
 	def get_queryset(self):
 		return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+class LentBooksListView(LoginRequiredMixin, generic.ListView):
+	"""Generic class-based view listing books on loan to users."""
+	model = BookInstance
+	template_name = 'catalog/lentbooks.html'
+	paginate_by = 10
+
+	def get_queryset(self):
+		return BookInstance.objects.filter(status__exact='o').order_by('due_back')
