bdc8da6ab0e457c097df5a4e920887ed3f76f81b
shreayan98c <shreayan98c@gmail.com>
2020-01-17 11:49:35 +0530

    Added regex validation for ISBN numbers

--- catalog/models.py
+++ catalog/models.py
@@ -3,12 +3,14 @@ from datetime import date
 from django.db import models
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 # Create your models here.
 
 class Genre(models.Model):
 	"""Model representing a book genre."""
-	name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+	name = models.CharField(max_length=200, 
+							help_text='Enter a book genre (e.g. Science Fiction)')
 
 	def __str__(self):
 		"""String for representing the Model object."""
@@ -17,7 +19,8 @@ class Genre(models.Model):
 
 class Language(models.Model):
 	"""Model representing a Language (e.g. English, French, Japanese, etc.)"""
-	name = models.CharField(max_length=200, help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+	name = models.CharField(max_length=200, 
+							help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
 
 	def __str__(self):
 		"""String for representing the Model object (in Admin site etc.)"""
@@ -30,15 +33,28 @@ class Book(models.Model):
 
 	# Foreign Key used because book can only have one author, but authors can have multiple books
 	# Author as a string rather than object because it hasn't been declared yet in the file
-	author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+	author = models.ForeignKey('Author', 
+							   on_delete=models.SET_NULL, 
+							   null=True)
 
-	summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
-	isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+	summary = models.TextField(max_length=1000, 
+							   help_text='Enter a brief description of the book')
+	
+	isbn = models.CharField('ISBN', 
+							max_length=13, 
+							help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>',
+							validators=[RegexValidator(
+								regex=r'^(?:ISBN(?:-13)?:? )?(?=[0-9]{13}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)97[89][- ]?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9]$', 
+								message='Please enter a valid phone number')])
 
 	# ManyToManyField used because genre can contain many books. Books can cover many genres.
 	# Genre class has already been defined so we can specify the object above.
-	genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
-	language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+	genre = models.ManyToManyField(Genre, 
+								   help_text='Select a genre for this book')
+
+	language = models.ForeignKey('Language', 
+								 on_delete=models.SET_NULL, 
+								 null=True)
 
 	def __str__(self):
 		"""String for representing the Model object."""
@@ -46,7 +62,8 @@ class Book(models.Model):
 
 	def get_absolute_url(self):
 		"""Returns the url to access a detail record for this book."""
-		return reverse('book-detail', args=[str(self.id)])
+		return reverse('book-detail', 
+					   args=[str(self.id)])
 
 	def display_genre(self):
 		"""Create a string for the Genre. This is required to display genre in Admin"""
@@ -56,11 +73,23 @@ class Book(models.Model):
 
 class BookInstance(models.Model):
 	"""Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
-	id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
-	book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True) 
+	id = models.UUIDField(primary_key=True, 
+						  default=uuid.uuid4, 
+						  help_text='Unique ID for this particular book across whole library')
+	
+	book = models.ForeignKey('Book', 
+							 on_delete=models.SET_NULL, 
+							 null=True) 
+	
 	imprint = models.CharField(max_length=200)
-	due_back = models.DateField(null=True, blank=True)
-	borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+	
+	due_back = models.DateField(null=True, 
+								blank=True)
+	
+	borrower = models.ForeignKey(User, 
+								 on_delete=models.SET_NULL, 
+								 null=True, 
+								 blank=True)
 
 	@property
 	def is_overdue(self):
@@ -95,16 +124,23 @@ class BookInstance(models.Model):
 class Author(models.Model):
 	"""Model representing an author."""
 	first_name = models.CharField(max_length=100)
+	
 	last_name = models.CharField(max_length=100)
-	date_of_birth = models.DateField(null=True, blank=True)
-	date_of_death = models.DateField('Died', null=True, blank=True)
+	
+	date_of_birth = models.DateField(null=True, 
+									 blank=True)
+	
+	date_of_death = models.DateField('Died', 
+									 null=True, 
+									 blank=True)
 
 	class Meta:
 		ordering = ['last_name', 'first_name']
 
 	def get_absolute_url(self):
 		"""Returns the url to access a particular author instance."""
-		return reverse('author-detail', args=[str(self.id)])
+		return reverse('author-detail', 
+					   args=[str(self.id)])
 
 	def __str__(self):
 		"""String for representing the Model object."""
