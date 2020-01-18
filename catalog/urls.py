4440cad48af7f044022ac29d4c3eeac290de7124
shreayan98c <shreayan98c@gmail.com>
2020-01-18 21:01:53 +0530

    Fixed a few absolute urls to relative urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -20,19 +20,26 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('lentbooks/', views.LentBooksListView.as_view(), name='borrowed'),
+
     path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+
     path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
     path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
     path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
+
     path('book/create/', views.BookCreate.as_view(), name='book_create'),
     path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book_update'),
     path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book_delete'),
+
     path('accounts/register/', views.register, name='register'),
     path('account/<int:pk>/', views.UserDetailView.as_view(), name='user_detail'),
     path('account/<int:pk>/update/', views.UserUpdate.as_view(), name='user_update'),
